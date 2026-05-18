0318f223fd1b688eafbabd1f0b603b822476fb3a
InessaIgumensev <igumensev@sitepark.com>
2026-05-18 15:35:19 +0200

    started blog component

--- src/Components/Blog.tsx
+++ src/Components/Blog.tsx
@@ -1,13 +1,57 @@
-export default function Blog(){
-    return(
-        <div className="blog">
-            <div className="blog-headline">
-            <h1> Mein Beispiel Blog</h1>
-            </div>
-            <div className="article">
-                <div className="picture-article"></div>
-                <div className="info-artikle"></div>
-            </div>
+import { BiDotsVerticalRounded } from "react-icons/bi";
+import { PiEyeLight } from "react-icons/pi";
+import { FiMessageSquare } from "react-icons/fi";
+import { CiHeart } from "react-icons/ci";
+import { IoIosContact } from "react-icons/io";
+import placeHolderPic from "../assets/time.avif";
+
+export default function Blog() {
+  return (
+    <div className="blog">
+      <div className="blog-headline">
+        <h1> Mein Beispiel Blog</h1>
+      </div>
+      <div className="blog-post-1">
+        <div className="picture-post-1">
+          <img src={placeHolderPic} alt="" />
         </div>
-    )
+        <div className="user-blog-post-1">
+          <div className="user-info-blog-post-1">
+            <div className="icon-blog-post-1">
+              {" "}
+              <IoIosContact />
+            </div>
+            <p>
+              {" "}
+              <span className="userName">Admin</span>
+            </p>
+            <span className="date-of-creation">2. Feb. 2023</span>
+            <span className="read-Time">1 Min. Lesezeit</span>
+            <button className="icon-read-More">
+              <BiDotsVerticalRounded />
+            </button>
+          </div>
+
+          <div className="info-blog-post-1">
+            <h2>Lorem</h2>
+            <span>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            </span>
+          </div>
+          <div className="footer-blog-post-1">
+            <div className="number-saw-it">
+              <PiEyeLight />
+            </div>
+            <div className="comments">
+              <FiMessageSquare />
+            </div>
+            <button className="likes">
+              <CiHeart />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
