63eaec1cf5d600d00f41b19b0ac0b7177240a239
InessaIgumensev <igumensev@sitepark.com>
2026-06-03 11:21:15 +0200

    argin and padding correctet in the subscribe componente

--- src/Components/Subscribe.tsx
+++ src/Components/Subscribe.tsx
@@ -1,17 +1,22 @@
 export function Subscribe() {
   return (
     <div className="subscribe-content">
-      <h3>Dem Club der Papierliebhaber beitreten</h3>
-      <p>Aktuelle Infos über Neuigkeiten</p>
-      
-      <form className="subscribe-form">
-        <div className="input-group">
+      <div className="subscribe-container">
+        <h3>Dem Club der Papierliebhaber beitreten</h3>
+        <p>Aktuelle Infos über Neuigkeiten</p>
+
+        <form className="subscribe-form">
           <label htmlFor="email">E-Mail-Adresse *</label>
           <input id="email" type="email" required />
-        </div>
-        
-        <button type="submit">Abonnieren</button>
-      </form>
+          <div className="abo-newsletter">
+            <input type="checkbox" id="newsletter" />
+            <label htmlFor="newsletter">
+              <span>Ja, ich möchte Ihren Newsletter abonnieren. *</span>
+            </label>
+          </div>
+          <button type="submit">Abonnieren</button>
+        </form>
+      </div>
     </div>
   );
 }
