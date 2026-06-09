76a7aceca017b88f4d3301523297afc3bd10465a
InessaIgumensev <igumensev@sitepark.com>
2026-06-09 14:07:59 +0200

    centered the contact component

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,36 +1,44 @@
 export function Contact() {
-  return <div className="contact">
-    
-    <div className="listing-products">
-      <h5>Shop</h5>
-      <p>Grußkarten</p>
-      <p>Notebooks</p>
-      <p>Geschenkpapier</p>
-      <p>Prints</p>
-      <p>Limitierte Auflagen</p>
+  return (
+    <div className="contact">
+      <div className="info-contact">
+        <div className="p-contacts">
+          <h1>Shop</h1>
+          <p>Grußkarten</p>
+          <p>Notebooks</p>
+          <p>Geschenkpapier</p>
+          <p>Prints</p>
+          <p>Limitierte Auflagen</p>
+        </div>
+      </div>
+      <div className="info-contact">
+        <div className="p-contacts">
+          <h1>AGB</h1>
+          <p>Versandrichtlinien</p>
+          <p>AGB</p>
+          <p>Zahlungsmethoden</p>
+          <p>FAQ</p>
+          <p>Cookies</p>
+          <p>Impressum</p>
+        </div>
+      </div>
+      <div className="info-contact">
+        <div className="p-contacts">
+          <h1>Öffnungszeiten</h1>
+          <p>Mon - Fri:7 - 22 Uhr</p>
+          <p>Sa.: 8 - 22 Uhr</p>
+          <p>So.: 8 - 23 Uhr</p>
+        </div>
+      </div>
+      <div className="info-contact">
+        <div className="p-contacts">
+          <h1>Schau vorbei</h1>
+          <p>Beispielstraße 12</p>
+          <p>12345 Beispiel</p>
+          <p>beispiel@web.com</p>
+          <p> Tel.: +49 (0) 123 4567</p>
+        </div>
+      </div>
     </div>
-    <div className="agb">
-      <h5>AGB</h5>
-      <p>Versandrichtlinien</p>
-      <p>AGB</p>
-      <p>Zahlungsmethoden</p>
-      <p>FAQ</p>
-      <p>Cookies</p>
-      <p>Impressum</p>
-    </div>
-    <div className="open">
-      <h5>Öffnungszeiten</h5>
-      <p>Mon - Fri:7 - 22 Uhr</p>
-      <p>Sa.: 8 - 22 Uhr</p>
-      <p>So.: 8 - 23 Uhr</p>
-    </div>
-    <div className="stop-by">
-      <h5>Schau vorbei</h5>
-      <p>Beispielstraße 12</p>
-      <p>12345 Beispiel</p>
-      <p>beispiel@web.com</p>
-      <p> Tel.: +49 (0) 123 4567</p>
-    </div>
-  </div>;
+  );
 }
-
