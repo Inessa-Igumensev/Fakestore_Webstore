2c8263e5a338341813fe13eb8a3dede77ceee173
Inessa Igumensev <igumensev.inessa@gmail.com>
2026-06-20 13:33:58 +0200

    added logout confirmation

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,20 +1,46 @@
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { PiShoppingCartBold } from "react-icons/pi";
 import { LuCircleUserRound } from "react-icons/lu";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Login from "./Components/Login";
+import Modal from "./Components/Modal";
 
 export const Navbar = () => {
   const [seen, setSeen] = useState<boolean>(false);
+  const [isLoggedIn,setIsLoggedIn] = useState<boolean>(false);
+  const [showDialog,setShowDialog] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
+const openLogoutDialog = () => {
+    setShowDialog(true); 
+  };
+
+  const handelLogout = () =>{
+    localStorage.removeItem("token");
+    localStorage.removeItem("role")
+    setIsLoggedIn(false);
+    setShowDialog(false);
+    console.log("Erfolgreich ausgeloggt");
+    navigate("/");
+  }
 
   function togglePop() {
     setSeen(!seen);
   }
 
-  const role = localStorage.getItem("role");
+const role = isLoggedIn ? localStorage.getItem("role") : null;
   const userPath = role === "admin" ? "/admin" : "/user";
 
+useEffect(() => {
+    if (localStorage.getItem("token") !== null) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false); 
+     }
+  }, [seen]); 
+  
   return (
     <div className="navbar">
       <Link to="/search">
@@ -45,13 +71,56 @@ export const Navbar = () => {
         <Link to={userPath}>
           <span className="iconUser"> {<LuCircleUserRound />}</span>
         </Link>
-        <button className="loginButton" onClick={togglePop}>
+        {isLoggedIn ? (
+            <button className="loginButton" onClick={openLogoutDialog}>
+          Abmelden
+        </button>
+        ) : ( 
+   <button className="loginButton" onClick={togglePop}>
           Anmelden
         </button>
+        )}
+     
         {seen ? <Login toggle={togglePop} /> : null}
         <Link to="/cart">
           <span className="iconCart">{<PiShoppingCartBold />}</span>
         </Link>
+
+        <Modal
+        isOpen={showDialog}
+        hasCloseBtn onClose={()=> setShowDialog(false)}
+        ariaLabelledBy="logout-dialog-title"
+        ariaDescribedBy="logout-dialog-description"
+        >
+          <div
+          className="del-container" // Nutzt dieselbe CSS-Struktur wie beim Löschen
+          onMouseDown={(event) => event.stopPropagation()}
+        >
+          <h2 id="logout-dialog-title">Abmelden?</h2>
+
+          <p id="logout-dialog-description">
+            Bist du sicher, dass du dich von deinem Konto abmelden möchtest?
+          </p>
+
+          <div className="modal-buttons">
+            <button
+              className="yes-btn"
+              type="button"
+              onClick={handelLogout}
+            >
+              Ja
+            </button>
+            <button
+              type="button"
+              className="no-btn"
+              onClick={() => setShowDialog(false)}
+            >
+              Nein
+            </button>
+          </div>
+        </div>
+        </Modal>
+
       </div>
     </div>
   );
