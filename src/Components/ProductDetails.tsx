3ea3e96040b22756ede0f1c68bb86857977a82ed
InessaIgumensev <igumensev@sitepark.com>
2026-06-24 17:35:35 +0200

    started product details

---
+++ src/Components/ProductDetails.tsx
@@ -0,0 +1,18 @@
+
+export default function ProductDetails(){
+    return(
+        <div className="product-details">
+            <div className="product-route">
+                <div className="product-routing">Start / Grußkarten / Ich bin ein Produkt</div>
+                <div className="product-back-more"> Zurück | Weiter</div>
+            </div>
+            <div className="product-information">
+                <div className="product-meta-dates">
+                    <div className="product-img"></div>
+                    <div className="product-description"></div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
